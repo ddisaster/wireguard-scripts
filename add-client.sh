48764b81ed7395570a6ac972e6c5a444668ecb7c
ddisaster <ddisaster@users.noreply.github.com>
2020-03-03 11:47:49 +0100

    Update add-client.sh

--- add-client.sh
+++ add-client.sh
@@ -2,22 +2,28 @@
 
 if [ $# -eq 0 ]
 then
-	echo "must pass a client name as an arg: add-client.sh new-client"
+	echo "usage: ${0} client-name"
 else
 	echo "Creating client config for: $1"
-	mkdir -p clients/$1
-	wg genkey | tee clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
-	key=$(cat clients/$1/$1.priv) 
-	ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
-	FQDN=$(hostname -f)
-	cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
-	echo $ip > last-ip.txt
-	cp SETUP.txt clients/$1/SETUP.txt
-	tar czvf clients/$1.tar.gz clients/$1
+	
+	source /etc/wireguard/config
+	mkdir -p /etc/wireguard/clients/$1
+	key="$(wg keygen)"
+	echo "${key}" > /etc/wireguard/clients/$1/$1.priv
+	echo "${key}" | wg pubkey > /etc/wireguard/clients/$1/$1.pub
+	this_ip=$(($(cat /etc/wireguard/last-ip.txt)+1))
+	echo "${this_ip}" > /etc/wireguard/last-ip.txt
+	ip="${cust_network}.${this_ip}"
+	cat /etc/wireguard/wg0-client.example.conf | \
+		sed -e "s/VPN_IP/${ip}/" | \
+		sed -e "s/PRIVKEY/${key}/" | \
+		sed -e "s/SERVER_PUBLIC/${server_public}/" | \
+		sed -e "s#ALLOWEDIPS#${cust_network}.0/24, ${cust_network}#" | \
+		sed -e "s/SERVER/${server_addr}/" \
+		> clients/$1/wg0.conf
 	echo "Created config!"
+	
 	echo "Adding peer"
-	sudo wg set wg0 peer $(cat clients/$1/$1.pub) allowed-ips $ip/32
+	wg set wg0 peer $(cat clients/$1/$1.pub) allowed-ips $ip/32
 	echo "Adding peer to hosts file"
-	echo $ip" "$1 | sudo tee -a /etc/hosts
-	sudo wg show
 fi
